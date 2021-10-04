5250db8e0c0bfea3c9e216e3f4accc1c795ce48b
Brahiam Tabares <dbtabaresv@uqvirtual.edu.co>
2021-10-04 00:03:30 -0500

    Se agrega el proyecto libreria al repositorio

---
+++ src/main/java/co/edu/uniquindio/software/libreria/LibreriaRestApplication.java
@@ -0,0 +1,14 @@
+package co.edu.uniquindio.software.libreria;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+@ApplicationScoped
+public class LibreriaRestApplication extends Application {
+
+}
